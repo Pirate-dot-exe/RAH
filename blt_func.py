10f3807e5e525b1cf8228d2b9e583b0975d705c3
PirateX20 <feliperangelm16dsm@gmail.com>
2023-04-24 21:35:21 -0300

    Pequenas atualizações dentro da função bluetooth

--- blt_func.py
+++ blt_func.py
@@ -1,3 +1,7 @@
+#TODO: 
+#Implementar/Adaptar função que faça o pareamento com o dispositivo escolhido 
+#Implementar função que permita o envio de dados ao dispositivo
+
 import bluetooth        #pybluez, talvez funcione apenas no win
 import subprocess
 import time
@@ -12,10 +16,19 @@ def search_new_devices():
     for addr, name in nearby_devices:
         print("address: ", addr, "name: ", name)
         device_list = device_list + "name: " + name + " address: " + addr + "\n"
+        find_services(addr, name)
     return nearby_devices, device_list
 
+def find_services(addr, name):
+    services = bluetooth.find_service(addr)
+    if len(services) <= 0:
+        print("no services found for "+ name)
+    else: 
+        for serv in services:
+            print(serv['name'])
+
 def try_connection(addr):
-    port = 1
+    port = 4
     passkey = "1111"
     print("trying blt connection to " + addr)
 
@@ -27,3 +40,19 @@ def try_connection(addr):
     except bluetooth.btcommon.BluetoothError as err:
         print("cannot connect bluetooth")
         pass
+#testes---------------------------------------------------------------------------------------------------
+def try_connection_socket(addr):
+    import socket
+
+    ports = range(1, 30)  # Números de porta a serem verificados
+    for port in ports:
+        print("trying to connect at port" + str(port) + "...")
+        sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        try:
+            sock.connect((addr, port))
+            print("Conexão estabelecida com sucesso!")
+            break
+        except socket.error as err:
+            print("nao foi possivel se conectar a porta ", port)
+        sock.close()
+#try_connection_socket(val_esp)
