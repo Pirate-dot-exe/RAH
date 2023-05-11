25343720d5180844ab946cb54e6e7feedd9a9f74
PirateX20 <feliperangelm16dsm@gmail.com>
2023-05-11 14:46:23 -0300

    Dv-3.d.04 (testando slider)

--- telas/screen_camera.py
+++ telas/screen_camera.py
@@ -1,7 +1,10 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.slider import Slider
 
 class CameraScreen(Screen):
     camera_resolution = ""
     def set_camera_resolution(self):
         self.camera_resolution = self.ids.input_teste.text
         print(self.camera_resolution)
+
+    
