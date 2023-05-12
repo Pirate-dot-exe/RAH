d7ffd41964f289641243e2037fbe2c894574c91d
PirateX20 <feliperangelm16dsm@gmail.com>
2023-05-11 22:19:46 -0300

    Dv-3.d.0c (sliders 3/6)

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ This Application is Using
 ~Kivy: 1.9.0
 
 ---A FAZER---
-+implementar barras deslizantes para manipular os valores de detecção em tempo de execução
+(*obrigatórios, +adicionais)
++integrar a faixa de detecção ao algoritmo de rastreamento
++integrar os dois conjuntos para estabelecer a faixa de detecção do rastreamento
++implementar mais um conjunto de barras deslizantes
 +implementar try/catch para que a aplicação não crashe quando não há imagem para detecção
 +Separar a busca de bluetooth em uma thread a parte impedindo que a interface gráfica pare de responder quando existem muitos dispositivos próximos
 
+~~+implementar barras deslizantes~~
 ~~*integrar a função de rastreamento do disco~~
 ~~*integrar a função de reconhecimento dos marcadores ArUco~~
 ~~*Fazer a mudança da camera para a da cv2~~
--- telas/screen_camera.py
+++ telas/screen_camera.py
@@ -6,55 +6,46 @@ from kivy.properties import NumericProperty
 
 class CameraScreen(Screen):
     camera_resolution = ""
-    red_value = NumericProperty(0)
-    green_value = NumericProperty(0)
-    blue_value = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super(CameraScreen, self).__init__(**kwargs)
-        self.custom_color_box = CustomColorBox()
-        self.ids.color_box_layout.add_widget(self.custom_color_box)
-        self.bind(red_value=self.update_color,
-                  green_value=self.update_color,
-                  blue_value=self.update_color
-            )
+        self.custom_color_box = self.ids.color_box
 
     def set_camera_resolution(self):
         self.camera_resolution = self.ids.input_teste.text
         print(self.camera_resolution)
 
-    def on_red_value(self, instance, value):
-        self.ids.red_value_slider = value
-    
-    def on_green_value(self, instance, value):
-        self.ids.green_value_slider = value
-
-    def on_blue_value(self, instance, value):
-        self.ids.blue_value_slider = value
-
-    def update_color(self, *args):
-        self.custom_color_box.update_color(
-            self.red_value,
-            self.green_value,
-            self.blue_value
+    def update_color(self):
+        print(self.ids.red_value.value)
+        self.ids.color_box.set_color(
+            self.ids.red_value.value,
+            self.ids.green_value.value,
+            self.ids.blue_value.value
         )
 
 class CustomColorBox(Widget):
+    red_value = NumericProperty(0)
+    green_value = NumericProperty(0)
+    blue_value = NumericProperty(0)
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super(CustomColorBox, self).__init__(**kwargs)
+        self.bind(
+            red_value=self.update_color,
+            green_value=self.update_color,
+            blue_value=self.update_color
+        )
         with self.canvas.before:
-            self.rect_color = Color(0, 0, 0, 1)
+            self.rect_color = Color(1, 0, 0, 1)
             self.rect = Rectangle(pos=self.pos, size=self.size)
         self.bind(pos=self.update_rect, size=self.update_rect)
-        self.update_color
 
     def update_rect(self, *args):
         self.rect.pos = self.pos
         self.rect.size = self.size
 
     def update_color(self, *args):
-        self.set_color(self.red_value/255, self.green_value/255, self.blue_value/255)
+        self.set_color(self.red_value, self.green_value, self.blue_value)
 
     def set_color(self, red, green, blue):
         self.rect_color.rgba = (red/255, green/255, blue/255, 1)
